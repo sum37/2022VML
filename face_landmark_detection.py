07c5a00366401270e4bbc6ae6f38505a486bc802
unknown <sum37@kaist.ac.kr>
2022-01-04 16:29:54 +0900

    facial detection with green number

--- face_landmark_detection.py
+++ face_landmark_detection.py
@@ -1,55 +1,24 @@
-#!/usr/bin/python
-# The contents of this file are in the public domain. See LICENSE_FOR_EXAMPLE_PROGRAMS.txt
-#
-#   This example program shows how to find frontal human faces in an image and
-#   estimate their pose.  The pose takes the form of 68 landmarks.  These are
-#   points on the face such as the corners of the mouth, along the eyebrows, on
-#   the eyes, and so forth.
-#
-#   The face detector we use is made using the classic Histogram of Oriented
-#   Gradients (HOG) feature combined with a linear classifier, an image pyramid,
-#   and sliding window detection scheme.  The pose estimator was created by
-#   using dlib's implementation of the paper:
-#      One Millisecond Face Alignment with an Ensemble of Regression Trees by
-#      Vahid Kazemi and Josephine Sullivan, CVPR 2014
-#   and was trained on the iBUG 300-W face landmark dataset (see
-#   https://ibug.doc.ic.ac.uk/resources/facial-point-annotations/):  
-#      C. Sagonas, E. Antonakos, G, Tzimiropoulos, S. Zafeiriou, M. Pantic. 
-#      300 faces In-the-wild challenge: Database and results. 
-#      Image and Vision Computing (IMAVIS), Special Issue on Facial Landmark Localisation "In-The-Wild". 2016.
-#   You can get the trained model file from:
-#   http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2.
-#   Note that the license for the iBUG 300-W dataset excludes commercial use.
-#   So you should contact Imperial College London to find out if it's OK for
-#   you to use this model file in a commercial product.
-#
-#
-#   Also, note that you can train your own models using dlib's machine learning
-#   tools. See train_shape_predictor.py to see an example.
-#
-#
-# COMPILING/INSTALLING THE DLIB PYTHON INTERFACE
-#   You can install dlib using the command:
-#       pip install dlib
-#
-#   Alternatively, if you want to compile dlib yourself then go into the dlib
-#   root folder and run:
-#       python setup.py install
-#
-#   Compiling dlib should work on any operating system so long as you have
-#   CMake installed.  On Ubuntu, this can be done easily by running the
-#   command:
-#       sudo apt-get install cmake
-#
-#   Also note that this example requires Numpy which can be installed
-#   via the command:
-#       pip install numpy
-
 import sys
 import os
 import dlib
 import glob
+import cv2  #opencv 사용
 
+#opencv에서 ESC 키입력 상수
+ESC_KEY = 27
+
+'''
+RGB > BGR or BGR > RGB 변환 
+dlib는 RGB 형태로 이미지를 사용하고
+openCV는 BGR 형태이므로 B와 R을 바꿔주는 함수가 필요하다.
+'''
+def swapRGB2BGR(rgb):
+    r, g, b = cv2.split(img)
+    bgr = cv2.merge([b,g,r])
+    return bgr
+'''
+매개변수가 3개여야 한다.
+'''
 if len(sys.argv) != 3:
     print(
         "Give the path to the trained shape predictor model as the first "
@@ -61,34 +30,64 @@ if len(sys.argv) != 3:
         "    http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2")
     exit()
 
+# 랜드마크 파일 경로
 predictor_path = sys.argv[1]
+# 이미지 경로
 faces_folder_path = sys.argv[2]
 
+# 얼굴 인식용 클래스 생성 (기본 제공되는 얼굴 인식 모델 사용)
 detector = dlib.get_frontal_face_detector()
+# 인식된 얼굴에서 랜드마크 찾기위한 클래스 생성 
 predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
-win = dlib.image_window()
 
+# 이미지를 화면에 표시하기 위한 openCV 윈도 생성
+cv2.namedWindow('Face')
+
+# 두번째 매개변수로 지정한 폴더를 싹다 뒤져서 jpg파일을 찾는다. 
 for f in glob.glob(os.path.join(faces_folder_path, "*.jpg")):
     print("Processing file: {}".format(f))
-    img = dlib.load_rgb_image(f)
-
-    win.clear_overlay()
-    win.set_image(img)
-
-    # Ask the detector to find the bounding boxes of each face. The 1 in the
-    # second argument indicates that we should upsample the image 1 time. This
-    # will make everything bigger and allow us to detect more faces.
+    
+    # 파일에서 이미지 불러오기
+    img = dlib.load_rgb_image(f)      
+    
+    #불러온 이미지 데이터를 R과 B를 바꿔준다.
+    cvImg = swapRGB2BGR(img)    
+    
+    # 얼굴 인식 두번째 변수 1은 업샘플링을 한번 하겠다는 얘기인데
+    # 업샘플링을하면 더 많이 인식할 수 있다고 한다.
+    # 다만 값이 커질수록 느리고 메모리도 많이 잡아먹는다.
+    # 그냥 1이면 될 듯. 
     dets = detector(img, 1)
+
+    # 인식된 얼굴 개수 출력 
     print("Number of faces detected: {}".format(len(dets)))
+    
+    # 이제부터 인식된 얼굴 개수만큼 반복하여 얼굴 윤곽을 표시할 것이다. 
     for k, d in enumerate(dets):
+        # k 얼굴 인덱스
+        # d 얼굴 좌표
         print("Detection {}: Left: {} Top: {} Right: {} Bottom: {}".format(
             k, d.left(), d.top(), d.right(), d.bottom()))
-        # Get the landmarks/parts for the face in box d.
-        shape = predictor(img, d)
-        print("Part 0: {}, Part 1: {} ...".format(shape.part(0),
-                                                  shape.part(1)))
-        # Draw the face landmarks on the screen.
-        win.add_overlay(shape)
 
-    win.add_overlay(dets)
-    dlib.hit_enter_to_continue()
+        # 인식된 좌표에서 랜드마크 추출 
+        shape = predictor(img, d)
+        print(shape.num_parts)
+        # num_parts(랜드마크 구조체)를 하나씩 루프를 돌린다.
+        for i in range(0, shape.num_parts):
+            # 해당 X,Y 좌표를 두배로 키워 좌표를 얻고
+            x = shape.part(i).x
+            y = shape.part(i).y
+
+            # 좌표값 출력
+            print(str(x) + " " + str(y))
+
+            # 이미지 랜드마크 좌표 지점에 인덱스(랜드마크번호, 여기선 i)를 putText로 표시해준다.
+            cv2.putText(cvImg, str(i), (x, y), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 0.3, (0, 255, 0))                    
+        # 랜드마크가 표시된 이미지를 openCV 윈도에 표시
+        cv2.imshow('Face', cvImg)
+
+    # 무한 대기를 타고 있다가 ESC 키가 눌리면 빠져나와 다음 이미지를 검색한다.
+    while True:
+        if cv2.waitKey(0) == ESC_KEY:
+            break;
+cv2.destroyWindow('Face')
--- st.py
+++ st.py
@@ -6,7 +6,7 @@ rows, cols = img.shape[:2]
 
 
 # 이미지의 중심점을 기준으로 90도 회전 하면서 0.5배 Scale
-M= cv2.getRotationMatrix2D((cols/2, rows/2),45, 1)
+M= cv2.getRotationMatrix2D((cols/3, rows/2),45, 0.5)
 
 dst = cv2.warpAffine(img, M,(cols, rows))
 
