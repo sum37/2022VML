fd00f8e094b87129a45a75b21f4909b409262a53
unknown <sum37@kaist.ac.kr>
2022-01-06 14:07:24 +0900

    load two images with red point

--- face_landmark_detection.py
+++ face_landmark_detection.py
@@ -6,7 +6,7 @@ import cv2
 
 ESC_KEY = 27
 
-def swapRGB2BGR(rgb):
+def swapRGB2BGR(rgb, img):
     r, g, b = cv2.split(img)
     bgr = cv2.merge([b,g,r])
     return bgr
@@ -28,59 +28,91 @@ faces_folder_path = sys.argv[2]
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 
-cv2.namedWindow('Face')
+#create window
+cv2.namedWindow('Face_1')
+cv2.namedWindow('Face_2')
 
-for f in glob.glob(os.path.join(faces_folder_path, "*.jpg")):
-    print("Processing file: {}".format(f))
-    
-    img = dlib.load_rgb_image(f)      
-    
-    cvImg = swapRGB2BGR(img)    
-    dets = detector(img, 1)
+#load two images    
+img1 = dlib.load_rgb_image("./me.jpg")      
+img2 = dlib.load_rgb_image("./winter.jpg")      
 
-    print("Number of faces detected: {}".format(len(dets)))
+cvImg1 = swapRGB2BGR(img1, img1)    
+dets1 = detector(img1, 1)
+
+print("Number of faces detected: {}".format(len(dets1)))
     
-    left_x=0
-    left_y=0
-    right_x=0
-    right_y=0
+left_x_1=0
+left_y_1=0
+right_x_1=0
+right_y_1=0
     
-    for k, d in enumerate(dets):
-        print("Detection {}: Left: {} Top: {} Right: {} Bottom: {}".format(
-            k, d.left(), d.top(), d.right(), d.bottom()))
+for k, d in enumerate(dets1):   
+    shape = predictor(img1, d) ##shape size가 68개 
+    print(shape.num_parts)
+    for i in range(0, shape.num_parts):
+        if i>=36 and i<=41:
+            x = shape.part(i).x
+            y = shape.part(i).y
+            print(str(x) + " " + str(y))
+            cv2.putText(cvImg1, str(i), (x, y), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 0.3, (0, 255, 0))
+            left_x_1+=x
+            left_y_1+=y
+        if i>=42 and i<=47:
+            x = shape.part(i).x
+            y = shape.part(i).y
+            print(str(x) + " " + str(y))
+            cv2.putText(cvImg1, str(i), (x, y), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 0.3, (0, 255, 0))
+            right_x_1+=x
+            right_y_1+=y
+        print(left_x_1/6)
+        print(left_y_1/6)
+        print(right_x_1/6)
+        print(right_y_1/6)   
         
-        print("Here: only eye")
-        
-        shape = predictor(img, d) ##shape size가 68개 
-        print(shape.num_parts)
-        for i in range(0, shape.num_parts):
-            if i>=36 and i<=41:
-                x = shape.part(i).x
-                y = shape.part(i).y
-                print(str(x) + " " + str(y))
-                cv2.putText(cvImg, str(i), (x, y), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 0.3, (0, 255, 0))
-                left_x+=x
-                left_y+=y
-            if i>=42 and i<=47:
-                x = shape.part(i).x
-                y = shape.part(i).y
-                print(str(x) + " " + str(y))
-                cv2.putText(cvImg, str(i), (x, y), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 0.3, (0, 255, 0))
-                right_x+=x
-                right_y+=y
-        print(left_x/6)
-        print(left_y/6)
-        print(right_x/6)
-        print(right_y/6)   
-        
-        cv2.circle(cvImg, (int(left_x/6),int(left_y/6)), 3, (0, 0, 255), -1)
-        cv2.circle(cvImg, (int(right_x/6),int(right_y/6)), 3, (0, 0, 255), -1)
+    cv2.circle(cvImg1, (int(left_x_1/6),int(left_y_1/6)), 3, (0, 0, 255), -1)
+    cv2.circle(cvImg1, (int(right_x_1/6),int(right_y_1/6)), 3, (0, 0, 255), -1)
                        
-        cv2.imshow('Face', cvImg)
+    cv2.imshow('Face_1', cvImg1)
 
+cvImg2 = swapRGB2BGR(img2, img2)    
+dets2 = detector(img2, 1)
 
-    while True:
-        if cv2.waitKey(0) == ESC_KEY:
-            break;
+print("Number of faces detected: {}".format(len(dets2)))
+    
+left_x_2=0
+left_y_2=0
+right_x_2=0
+right_y_2=0
+    
+for k, d in enumerate(dets2):   
+    shape = predictor(img2, d) ##shape size가 68개 
+    print(shape.num_parts)
+    for i in range(0, shape.num_parts):
+        if i>=36 and i<=41:
+            x = shape.part(i).x
+            y = shape.part(i).y
+            print(str(x) + " " + str(y))
+            cv2.putText(cvImg2, str(i), (x, y), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 0.3, (0, 255, 0))
+            left_x_2+=x
+            left_y_2+=y
+        if i>=42 and i<=47:
+            x = shape.part(i).x
+            y = shape.part(i).y
+            print(str(x) + " " + str(y))
+            cv2.putText(cvImg2, str(i), (x, y), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 0.3, (0, 255, 0))
+            right_x_2+=x
+            right_y_2+=y
+        print(left_x_2/6)
+        print(left_y_2/6)
+        print(right_x_2/6)
+        print(right_y_2/6)   
+        
+    cv2.circle(cvImg2, (int(left_x_2/6),int(left_y_2/6)), 3, (0, 0, 255), -1)
+    cv2.circle(cvImg2, (int(right_x_2/6),int(right_y_2/6)), 3, (0, 0, 255), -1)
+                       
+cv2.imshow('Face_2', cvImg2)
+while True:
+    if cv2.waitKey(0) == ESC_KEY:
+        break;
         
 cv2.destroyWindow('Face')
