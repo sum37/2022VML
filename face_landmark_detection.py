112c74e954f3fb077265ed689e54832730f170a9
unknown <sum37@kaist.ac.kr>
2022-01-20 13:25:25 +0900

    finish similarity transformation

--- face_landmark_detection.py
+++ face_landmark_detection.py
@@ -1,7 +1,6 @@
 import sys
-import os
+from turtle import left, right, width
 import dlib
-import glob
 import cv2
 import numpy as np
 
@@ -29,31 +28,20 @@ faces_folder_path = sys.argv[2]
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 
-#create window
-# cv2.namedWindow('Face_1')
-# cv2.namedWindow('Face_2')
-cv2.namedWindow('Combine')
-
 #load two images    
-img1 = dlib.load_rgb_image("./winter.jpg")      
-img2 = dlib.load_rgb_image("./me.jpg")
+img1 = dlib.load_rgb_image("./taeyeon.jpg")      
+img2 = dlib.load_rgb_image("./winter.jpg")
 
-print("aa", len(img1))
+img2=cv2.resize(img2, (img2.shape[1]//3, img2.shape[0]//3))
 
-
-#match picture size
+#size
 width1=img1.shape[1]
 height1=img1.shape[0]
 width2=img2.shape[1]
 height2=img2.shape[0]
 
-if width2>=width1:
-    img2=cv2.resize(img2, (width1, height1))
-else:
-    img1=cv2.resize(img1, (width2, height2))
-
 #eye detecting, img1
-cvImg1 = swapRGB2BGR(img1, img1)    
+img1 = swapRGB2BGR(img1, img1)
 dets1 = detector(img1, 1)
     
 left_x_1=0
@@ -67,20 +55,18 @@ for k, d in enumerate(dets1):
         if i>=36 and i<=41:
             x = shape.part(i).x
             y = shape.part(i).y
-            cv2.putText(cvImg1, str(i), (x, y), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 0.3, (0, 255, 0))
+            # cv2.putText(img1, str(i), (x, y), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 0.3, (0, 255, 0))
             left_x_1+=x
             left_y_1+=y
         if i>=42 and i<=47:
             x = shape.part(i).x
             y = shape.part(i).y
-            cv2.putText(cvImg1, str(i), (x, y), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 0.3, (0, 255, 0))
+            # cv2.putText(img1, str(i), (x, y), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 0.3, (0, 255, 0))
             right_x_1+=x
             right_y_1+=y
         
-    cv2.circle(cvImg1, (int(left_x_1/6),int(left_y_1/6)), 3, (0, 0, 255), -1)
-    cv2.circle(cvImg1, (int(right_x_1/6),int(right_y_1/6)), 3, (0, 0, 255), -1)
-                       
-    # cv2.imshow('Face_1', cvImg1)
+    # cv2.circle(img1, (int(left_x_1/6),int(left_y_1/6)), 3, (0, 0, 255), -1)
+    # cv2.circle(img1, (int(right_x_1/6),int(right_y_1/6)), 3, (0, 0, 255), -1)
 
 left_x_1=left_x_1/6
 left_y_1=left_y_1/6
@@ -88,7 +74,7 @@ right_x_1=right_x_1/6
 right_y_1=right_y_1/6
 
 #eye detecting, img2
-cvImg2 = swapRGB2BGR(img2, img2)    
+img2 = swapRGB2BGR(img2, img2)    
 dets2 = detector(img2, 1)
 
 left_x_2=0
@@ -102,73 +88,98 @@ for k, d in enumerate(dets2):
         if i>=36 and i<=41:
             x = shape.part(i).x
             y = shape.part(i).y
-            cv2.putText(cvImg2, str(i), (x, y), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 0.3, (0, 255, 0))
+            # cv2.putText(img2, str(i), (x, y), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 0.3, (0, 255, 0))
             left_x_2+=x
             left_y_2+=y
         if i>=42 and i<=47:
             x = shape.part(i).x
             y = shape.part(i).y
-            cv2.putText(cvImg2, str(i), (x, y), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 0.3, (0, 255, 0))
+            # cv2.putText(img2, str(i), (x, y), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 0.3, (0, 255, 0))
             right_x_2+=x
             right_y_2+=y
-                   
-    cv2.circle(cvImg2, (int(left_x_2/6),int(left_y_2/6)), 3, (0, 0, 255), -1)
-    cv2.circle(cvImg2, (int(right_x_2/6),int(right_y_2/6)), 3, (0, 0, 255), -1)
-
+                         
+    # cv2.circle(img2, (int(left_x_2/6),int(left_y_2/6)), 3, (0, 0, 255), -1)
+    # cv2.circle(img2, (int(right_x_2/6),int(right_y_2/6)), 3, (0, 0, 255), -1)
+    
 left_x_2=left_x_2/6
 left_y_2=left_y_2/6
 right_x_2=right_x_2/6
-right_y_2=right_y_2/6
-                         
-# cv2.imshow('Face_2', cvImg2)
+right_y_2=right_y_2/6                       
 
-#간격 비교
+#comparing distance between eyes
 dis1= ((left_x_1-right_x_1)**2+(left_y_1-right_y_1)**2)**0.5
 dis2= ((left_x_2-right_x_2)**2+(left_y_2-right_y_2)**2)**0.5
 
-#비율 따지기 ....
-p=dis1/dis2
-#img2=cv2.resize(img2, None, fx=1/p, fy=1/p)
+#비율 따지고 큰 것에 맞추기
+if dis1>=dis2:
+    p=dis1/dis2
+    img2=cv2.resize(img2, (int(width2*p), int(height2*p)))
+    newleft_x_2=left_x_2*p
+    newleft_y_2=left_y_2*p
+    newleft_x_1=left_x_1
+    newleft_y_1=left_y_1
+
+else:
+    p=dis2/dis1
+    img1=cv2.resize(img1, (int(width1*p), int(height1*p)))
+    newleft_x_1=left_x_1*p
+    newleft_y_1=left_y_1*p
+    newleft_x_2=left_x_2
+    newleft_y_2=left_y_2
+
+#새로운 사이즈
+width1=img1.shape[1]
+height1=img1.shape[0]
+width2=img2.shape[1]
+height2=img2.shape[0]
 
 #vector 생성
 img1_vector=[right_x_1-left_x_1, right_y_1-left_y_1]
 img2_vector=[right_x_2-left_x_2, right_y_2-left_y_2]
-print("here vector")
-print(img1_vector)
-print(img2_vector)
 
 #vector간 각도 구하기
 unit_img1_vector=img1_vector/np.linalg.norm(img1_vector)
 unit_img2_vector=img2_vector/np.linalg.norm(img2_vector)
 dot_product=np.dot(unit_img1_vector, unit_img2_vector)
-print(unit_img1_vector)
-print(unit_img2_vector)
 angle=np.arccos(dot_product)
-print("here angle")
-print(angle)
 
 #rotate, center: eye
 rows, cols=img2.shape[:2]
 N=cv2.getRotationMatrix2D((left_x_2, left_y_2), angle*180/3.14, 1)
-rotation_img2=cv2.warpAffine(img2, N, (cols, rows))
+img2=cv2.warpAffine(img2, N, (width2, height2))
 
-#x1-x2만큼 이동해야함
-diff_x=left_x_1-left_x_2
-diff_y=left_y_1-left_y_2
+#padding 가로, 세로 결정하기
+sizelist=[width1, width2, height1, height2]
+newsize=max(sizelist)
+
+#사진을 겹치기 위해서 필요한 값들 .... 
+x_padding1 = (newsize - width1) // 2
+y_padding1 = (newsize - height1) // 2
+x_padding2 = (newsize - width2) // 2
+y_padding2 = (newsize - height2) // 2
+
+# x1-x2만큼 이동해야함
+diff_x=newleft_x_1-newleft_x_2+x_padding1-x_padding2
+diff_y=newleft_y_1-newleft_y_2+y_padding1-y_padding2
 M=np.float32([[1,0,diff_x], [0,1,diff_y]])
-translation_img2=cv2.warpAffine(rotation_img2,M,(cols, rows))
+img2=cv2.warpAffine(img2,M,(width2, height2))
+
+#making black img
+blackimg1=np.full((newsize, newsize, 3), (0,0,0), dtype=np.uint8)
+blackimg2=np.full((newsize, newsize, 3), (0,0,0), dtype=np.uint8)
+
+
+# copy img image into center of result image
+blackimg1[y_padding1:y_padding1+height1, x_padding1:x_padding1+width1] = img1
+blackimg2[y_padding2:y_padding2+height2, x_padding2:x_padding2+width2] = img2
 
 #blending
 alpha=0.5
-
-img3=img1*alpha+translation_img2*(1-alpha)
+img3=blackimg1*alpha+blackimg2*(1-alpha)
 img3=img3.astype(np.uint8)
 
-#show img3
-cvImg3 = swapRGB2BGR(img3, img3) 
-cv2.imshow('Combine', cvImg3)
+cv2.imshow('combine', img3)
 
-#break if 
 while True:
     if cv2.waitKey(0) == ESC_KEY:
         break;
